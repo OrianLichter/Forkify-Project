8eced224064f2e4631f7bb9c489724b592cd1644
Orian Lichter <98488363+OrianLichter@users.noreply.github.com>
2023-03-22 21:56:00 +0200

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '33e89e96-a011-4f80-96ef-2285212ae68b';
+export const MODEL_CLOSE_SEC = 2.5;
